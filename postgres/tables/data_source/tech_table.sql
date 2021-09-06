124c4986ad2a02a3f6936efd8e7a4a2cad0149e7
Aleksbielarus <aleksbielarus@gmail.com>
2021-09-06 17:12:13 +0300

    modified tech_table

--- postgres/tables/data_source/tech_table.sql
+++ postgres/tables/data_source/tech_table.sql
@@ -8,5 +8,6 @@ drop table if exists data_source.tech_table;
 CREATE TABLE data_source.tech_table
 	(
      id int not null default nextval('data_source.seq_tech_table'),
-     parsing_date timestamp 
+     parsing_date date default CURRENT_DATE,
+     parsing_datetime timestamp WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP
 	);

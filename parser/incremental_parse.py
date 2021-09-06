f34c8de54aed01f965344be7a21c11f746fab640
Aleksbielarus <aleksbielarus@gmail.com>
2021-09-06 16:38:26 +0300

    preparation v2

--- parser/incremental_parse.py
+++ parser/incremental_parse.py
@@ -1,1 +1,12 @@
 import Connection
+
+# postgresql connection check
+dbname = 'postgres'
+if __name__ == "__main__":
+    db = Connection.connect(dbname)
+    cursor = db.cursor()
+    cursor.execute("select * from data_source.main_page_results limit 10")
+    records = cursor.fetchall()
+    for i in records:
+        print(i)
+    db.close()
